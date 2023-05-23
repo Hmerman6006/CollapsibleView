13fd82186b948095b8b8852f2fd142facae8e72b
MermanH <56772372+Hmerman6006@users.noreply.github.com>
2023-05-23 16:43:47 +0200

    Create CollapsibleViewAnimationUtil.kt
    First commit.

---
+++ CollapsibleViewAnimationUtil.kt
@@ -0,0 +1,47 @@
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.widget.LinearLayout.LayoutParams
+
+object CollapsibleViewAnimationUtil {
+    fun expand(v: View) {
+        v.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        val targtetHeight = v.measuredHeight
+        v.layoutParams.height = 0
+        v.visibility = View.VISIBLE
+        val a: Animation = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                v.layoutParams.height =
+                    if (interpolatedTime == 1f) LayoutParams.WRAP_CONTENT else (targtetHeight * interpolatedTime).toInt()
+                v.requestLayout()
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+        a.duration = (targtetHeight / v.context.resources.displayMetrics.density).toInt().toLong()
+        v.startAnimation(a)
+    }
+
+    fun collapse(v: View) {
+        val initialHeight = v.measuredHeight
+        val a: Animation = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                if (interpolatedTime == 1f) {
+                    v.visibility = View.GONE
+                } else {
+                    v.layoutParams.height =
+                        initialHeight - (initialHeight * interpolatedTime).toInt()
+                    v.requestLayout()
+                }
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+        a.duration = (initialHeight / v.context.resources.displayMetrics.density).toInt().toLong()
+        v.startAnimation(a)
+    }
+}
